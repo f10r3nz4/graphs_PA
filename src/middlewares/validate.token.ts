2efee9194d5772c383e11b790ad1caef586525b9
Fio <48447051+f10r3nz4@users.noreply.github.com>
2022-11-08 15:15:02 +0100

    comments on validation

--- src/middlewares/validate.token.ts
+++ src/middlewares/validate.token.ts
@@ -3,6 +3,7 @@ import { Code } from '../enum/code.enum';
 import jwt from 'jsonwebtoken';
 import { CustomRequest, User } from '../interface/user';
 
+//esporto una costante che mi certifica il token valido dell'utente e quindi la sua corretta autenticazione
 export const validateToken = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('Authorization');
     if(token === undefined) {
@@ -10,9 +11,12 @@ export const validateToken = async (req: Request, res: Response, next: NextFunct
             message: 'Unauthorized'
         })
     } else {
+        //prendo la chiave jwt dall'env
         const secret = process.env.JWT_SECRET;
         if(secret !== undefined) {
+            //verifico il token come chiave e token generato
             const tokenVerified = jwt.verify(token, secret);
+            //associo il token all'user autenticato
             const loggedUser = tokenVerified as User;
             (req as CustomRequest).user = {
                 id: loggedUser.id,
