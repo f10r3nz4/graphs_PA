ae2a0e974153a49876563cf5e0fdc44e2ce814c1
Fio <48447051+f10r3nz4@users.noreply.github.com>
2022-11-08 11:39:36 +0100

    comments on get nodes

--- src/controller/graph.controller/getNodes.ts
+++ src/controller/graph.controller/getNodes.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { daoNodes } from "../dao/dao.nodes/dao.nodes";
 
+//restituisce i nodi presenti nel DB tramite DAO
 export const getNodes = async (req: Request, res: Response) => {
     const dao: daoNodes = new daoNodes();
     const nodes = await dao.getNodes();
