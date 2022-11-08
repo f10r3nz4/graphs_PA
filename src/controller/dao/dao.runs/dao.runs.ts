f996601467d3f9654c3a5158350f5f0bf2ac1e87
Fio <48447051+f10r3nz4@users.noreply.github.com>
2022-11-08 15:03:58 +0100

    comments on dao runs

--- src/controller/dao/dao.runs/dao.runs.ts
+++ src/controller/dao/dao.runs/dao.runs.ts
@@ -7,12 +7,15 @@ import { ResultSet } from "../../user.controller/user.controller";
 export class daoRuns {
     pool;
 
+    //collegamento DB
     constructor() {
         this.pool = connection();
     }
 
+    //funzione per aggiungere la nuova esecuzione le informazioni relative
     async addRun(cost: number, idGraph: number, nodeStart: String, nodeGoal: String, time: number, optimalCost: number) {
         
+        //aggiungo l'esecuzione alla sua tabella tramite query
         await this.pool.query(
             QUERY.ADD_RUN,
             [
@@ -26,11 +29,14 @@ export class daoRuns {
         );
     }
 
+    //recupera le esecuzioni salvate sul DB
     async getRuns(): Promise<runI[]> {
         let runs: runI[] = [];
+        //recupero con una select le runs esistenti
         const rs: ResultSet = await this.pool.query(
             QUERY.SELECT_RUNS
         );
+        //salvo le informazioni secondo l'interfaccia
         (rs[0] as runI[]).map((run: any) => {
             const nameNodeStart = run.node_start.slice(0,-2);
             const nameNodeGoal = run.node_goal.slice(0,-2);
