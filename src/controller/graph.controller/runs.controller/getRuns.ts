f0376ea1e8a19a19b28a196c82993ed0d228f80a
Fio <48447051+f10r3nz4@users.noreply.github.com>
2022-11-08 12:05:05 +0100

    comments get runs

--- src/controller/graph.controller/runs.controller/getRuns.ts
+++ src/controller/graph.controller/runs.controller/getRuns.ts
@@ -3,6 +3,7 @@ import { Code } from "../../../enum/code.enum";
 import { CustomRequest } from "../../../interface/user";
 import { daoRuns } from "../../dao/dao.runs/dao.runs";
 
+//restituisce tutte le esecuzioni salvate nel DB tramite DAO
 export const getRuns = async (req: Request, res: Response) => {
     const daoRun: daoRuns = new daoRuns();
     const runs = await daoRun.getRuns();
