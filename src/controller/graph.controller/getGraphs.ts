7be3fcffa968d381141ff46ed4e9076a0632fc44
Fio <48447051+f10r3nz4@users.noreply.github.com>
2022-11-08 11:47:44 +0100

    comments on get graphs

--- src/controller/graph.controller/getGraphs.ts
+++ src/controller/graph.controller/getGraphs.ts
@@ -3,24 +3,24 @@ import { Code } from "../../enum/code.enum";
 import { LinkI } from "../../interface/graph";
 import { CustomRequest } from "../../interface/user";
 import { daoGraphs, graphI, nodeI } from "../dao/dao.graphs/dao.graphs";
-
+//recupera tutti i grafi creati dall'utente loggato
 export const getGraphs = async (req: Request, res: Response) => {
     const email = (req as CustomRequest).user.email;
-    const daoGraph: daoGraphs = new daoGraphs();
+    const daoGraph: daoGraphs = new daoGraphs(); //recupera i grafi dal DB con DAO
 
     if (email === undefined) {
         return res.status(Code.BAD_REQUEST).json({
             message: 'No email found'
         });
     }
-
+    //salvo nella costante le informazioni del grafo recuperate dal DB tramite l'email dell'utente loggato
     const graphs = await daoGraph.getGraphsByEmail(email);
 
     return res.status(200).json({
         graphs: graphs
     })
 }
-
+//restituisce i grafi filtrati in AND tra numero nodi e archi
 export const getFilteredGraphs = async (req: Request, res: Response) => {
     const email = (req as CustomRequest).user.email;
     const daoGraph: daoGraphs = new daoGraphs();
@@ -39,7 +39,7 @@ export const getFilteredGraphs = async (req: Request, res: Response) => {
     }
 
     const graphs = await daoGraph.getGraphsByEmail(email);
-
+    //salvo in una costante i grafi filtrati
     const filteredGraphs = graphs.filter((graph: graphI) => {
         if(graph.nodes.length === numberOfNodes && graph.links.length === numberOfLinks) {
             return graph;
